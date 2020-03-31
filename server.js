4ab1af03ab339456d29867a4415870b10af40ca4
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-31 09:12:25 +0900

    done chat

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const http = require("http");
+const cors = require("cors");
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const router = require("./router");
 
@@ -8,28 +11,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(cors());
 app.use(router);
 
-const { addUser, removerUser, getUser, getUsersInRoom } = require("./users");
-
-io.on("connection", socket => {
+io.on("connect", socket => {
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) {
-      return callback(error);
-    }
+    if (error) return callback(error);
+
+    socket.join(user.room);
 
     socket.emit("message", {
       user: "admin",
-      text: `${user.name}, welcome to room ${user.room}`
+      text: `${user.name}, welcome to room ${user.room}.`
     });
-
     socket.broadcast
       .to(user.room)
       .emit("message", { user: "admin", text: `${user.name} has joined!` });
 
-    socket.join(user.room);
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
 
     callback();
   });
@@ -43,10 +47,21 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User has left!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
   });
 });
 
-const PORT = process.env.PORT || 4000;
-
-server.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+server.listen(process.env.PORT || 4000, () =>
+  console.log(`Server has started.`)
+);
--- users.js
+++ users.js
@@ -8,9 +8,8 @@ const addUser = ({ id, name, room }) => {
     user => user.room === room && user.name === name
   );
 
-  if (existingUser) {
-    return { error: "Username is taken" };
-  }
+  if (!name || !room) return { error: "Username and room are required." };
+  if (existingUser) return { error: "Username is taken." };
 
   const user = { id, name, room };
 
@@ -19,20 +18,14 @@ const addUser = ({ id, name, room }) => {
   return { user };
 };
 
-const removerUser = id => {
+const removeUser = id => {
   const index = users.findIndex(user => user.id === id);
 
-  if (index !== -1) {
-    return users.splice(index, 1)[0];
-  }
+  if (index !== -1) return users.splice(index, 1)[0];
 };
 
-const getUser = id => {
-  users.find(user => user.id === id);
-};
+const getUser = id => users.find(user => user.id === id);
 
-const getUsersInRoom = room => {
-  users.filter(user => user.room === room);
-};
+const getUsersInRoom = room => users.filter(user => user.room === room);
 
-module.exports = { addUser, removerUser, getUser, getUsersInRoom };
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
